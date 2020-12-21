8d8186c52bea6fc6efdf8ae2cf1e14caed0c222a
Alextico 612 <alextico612@gmail.com>
2020-12-21 19:23:48 +0100

    Empezado

--- Proyecto1/MainWindow.xaml.cs
+++ Proyecto1/MainWindow.xaml.cs
@@ -7,9 +7,28 @@ namespace Proyecto1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Pelicula[] peliculasJuego = new Pelicula[5];
+        int contador = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void atrasImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (contador == 0) contador = peliculasJuego.Length - 1;
+            else contador--;
+            jugarDockPanel.DataContext = peliculasJuego[contador];
+            contadorTextBlock.Text = (contador+1)+" / "+peliculasJuego.Length;
+        }
+
+        private void adelanteImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (contador == peliculasJuego.Length-1) contador =0;
+            else contador++;
+            jugarDockPanel.DataContext = peliculasJuego[contador];
+            contadorTextBlock.Text = (contador + 1) + " / " + peliculasJuego.Length;
+
         }
     }
 }
---
+++ Proyecto1/Pelicula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Proyecto1
+{
+    class Pelicula
+    {
+        public string titulo, pista, imagen, dificultad, genero;
+
+        public Pelicula(String titulo, String pista, String imagen, String dificultad, String genero) {
+            this.titulo = titulo;
+            this.pista = pista;
+            this.imagen = imagen;
+            this.dificultad = dificultad;
+            this.genero = genero;
+        }
+
+    }
+}
